f727e437a32bcf53361c6538d98f66e845bc3639
KC Dorian <doriankoanda@gmail.com>
2026-08-10 23:09:15 +0000

    feat(web): add Vercel Web Analytics to landing page

--- kiyoshi-site/app/layout.tsx
+++ kiyoshi-site/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat, Inter, JetBrains_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -34,6 +35,7 @@ export default function RootLayout({
         className={`${montserrat.variable} ${inter.variable} ${jetbrains.variable} font-body antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
